1e966e41cc6cb521c4efad4c2c566204f1591eb4
kailinghoo <146651600+kailinghoo@users.noreply.github.com>
2023-10-16 14:51:09 +0000

    Add exercise 6

--- c_bootcamp_week2/lesson1/exercises/exercise6.c
+++ c_bootcamp_week2/lesson1/exercises/exercise6.c
@@ -3,7 +3,9 @@
 int main(){
     int num = 0;
     while (num != -1){
-        
+        printf("Enter a number");
+        scanf("%d", &num);
     }
+    printf("Code has been terminated\n");
     return 0;
 }
