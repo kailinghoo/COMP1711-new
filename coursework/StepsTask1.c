fcdace99fafac27e758e0894df5d5ee69c9a731f
kailinghoo <146651600+kailinghoo@users.noreply.github.com>
2023-10-26 10:26:54 +0000

    save coursework

--- coursework/StepsTask1.c
+++ coursework/StepsTask1.c
@@ -43,6 +43,17 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 // Complete the main function
 int main() {
-
-
+    char filename [] = "FitnessData_2023.csv";
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("");
+        return 1;
+    }
+    int buffer_size = 100;
+    char line_buffer[buffer_size];
+    while (fgets(line_buffer, buffer_size, file) != NULL) {
+        tokeniseRecord(line_buffer,",")
+    }
+    fclose(file);
+    return 0;
 }
