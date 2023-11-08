70f29cb4dc3bfb383e159c877e113abdc8d49df3
kailinghoo <146651600+kailinghoo@users.noreply.github.com>
2023-11-08 20:13:58 +0000

    menu F

--- coursework/test.c
+++ coursework/test.c
@@ -91,38 +91,78 @@ int optionE(FITNESS_DATA fitness[1000], int counter) {
     return 0;
 }
 
+int optionF(FITNESS_DATA fitness[1000], int counter) {
+    char date[11];
+	char timeStart[6];
+    char timeEnd[6];
+    char tempStart[6];
+    char tempEnd[6];
+    int highest = 0;
+    int counter2 = 0;
+    for (int i=0; i<counter; i++){
+        if (fitness[i].steps > 500){
+            strcpy(date, fitness[i].date);
+            strcpy(tempStart, fitness[i].time);
+            printf("%s",tempStart);
+            counter2++;
+            for (int j=i+1; j<counter;j++){
+                if (fitness[j].steps > 500){
+                    counter2++;
+                    strcpy(tempEnd,fitness[j].time);
+                }
+                else{
+                    if (counter2 > highest){
+                        strcpy(timeEnd, tempEnd);
+                        strcpy(timeStart, tempStart);
+                        highest = counter2;
+                        counter2 = 0;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    printf("Longest period start: %s %s\n", date, timeStart);
+    printf("Longest period end: %s %s\n", date, timeEnd);
+    return 0;
+}
+
 // Complete the main function
 int main() {
     char option;
+    option=Menu();
 
+    char filename [100];
     FITNESS_DATA fitness[1000];
     int buffer_size = 500;
     char line_buffer[buffer_size];
     int counter = 0;
 
-    char filename [] = "FitnessData_2023.csv";
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        perror("");
-        return 1;
-    }
-    
-    char date[11];
-    char time[6];
-    char steps[10];
-
-    while (fgets(line_buffer, buffer_size, file) != NULL) {
-        tokeniseRecord(line_buffer,",",date,time,steps);
-        strcpy(fitness[counter].date,date);
-        strcpy(fitness[counter].time,time);
-        fitness[counter].steps = atoi(steps);
-        counter ++;
-    }
-    fclose(file);
-
-    option=Menu();
-
     while (option != 'Q'){
+        if (option == 'A'){
+            printf("Input filename: ");
+            scanf("%s", filename);
+            FILE *file = fopen(filename, "r");
+            if (file == NULL) {
+                printf("Error: Could not find or open the file.\n");
+                return 1;
+            }
+
+            char date[11];
+            char time[6];
+            char steps[10];
+
+            while (fgets(line_buffer, buffer_size, file) != NULL) {
+                tokeniseRecord(line_buffer,",",date,time,steps);
+                strcpy(fitness[counter].date,date);
+                strcpy(fitness[counter].time,time);
+                fitness[counter].steps = atoi(steps);
+                counter ++;
+            }
+            fclose(file);
+            Menu();
+            scanf("%c",&option);
+        }
         if (option == 'B'){
             optionB(counter);
         }
@@ -134,6 +174,9 @@ int main() {
         }
         else if (option =='E'){
             optionE(fitness, counter);
+        }
+        else if (option == 'F'){
+            optionF(fitness, counter);
         }
         else{
             printf("Invalid choice. Try again.\n");
