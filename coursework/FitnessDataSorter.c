134e08680a9bd1d97981efd09cc9f90d2da309de
kailinghoo <146651600+kailinghoo@users.noreply.github.com>
2023-11-09 16:47:55 +0000

    Add task 3

---
+++ coursework/FitnessDataSorter.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define an appropriate struct
+typedef struct {
+	char date[11];
+	char time[6];
+	int steps;
+} FITNESS_DATA;
+
+// Define any additional variables here
+
+
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+}
+
+// Complete the main function
+int main() {
+    FITNESS_DATA fitness[1000];
+    int buffer_size = 500;
+    char line_buffer[buffer_size];
+    int counter = 0;
+
+    char filename [1000];
+    printf("Input filename: ");
+    scanf("%s", filename);
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error: invalid file");
+        return 1;
+    }
+    
+    char date[11];
+	char time[6];
+	char steps[10];
+
+    while (fgets(line_buffer, buffer_size, file) != NULL) {
+        tokeniseRecord(line_buffer,",",date,time,steps);
+        if (date != "" && time != "" && steps!= ""){
+            strcpy(fitness[counter].date,date);
+            strcpy(fitness[counter].time,time);
+            fitness[counter].steps = atoi(steps);
+            counter ++;
+        }
+        else{
+            printf("Error: invalid file");
+            return 1;
+        }
+    }
+    fclose(file);
+    return 0;
+}
--- coursework/StepCounter.c
+++ coursework/StepCounter.c
@@ -42,7 +42,7 @@ char Menu() {
     printf("F: Find the longest continuous period where the step count is above 500 steps\n");
     printf("Q: Quit\n");
     printf("Enter choice: ");
-    scanf("%c", &option);
+    scanf("%c",&option);
     return option;
 }
 
@@ -91,54 +91,99 @@ int optionE(FITNESS_DATA fitness[1000], int counter) {
     return 0;
 }
 
-int optionQ(){
-    exit(0);
+int optionF(FITNESS_DATA fitness[1000], int counter) {
+    char date[11];
+	char timeStart[6];
+    char timeEnd[6];
+    char tempStart[6];
+    char tempEnd[6];
+    int highest = 0;
+    int counter2 = 0;
+    for (int i=0; i<counter; i++){
+        if (fitness[i].steps > 500){
+            strcpy(date, fitness[i].date);
+            strcpy(tempStart, fitness[i].time);
+            printf("%s\n",tempStart);
+            for (int j=i; j<counter;j++){
+                if (fitness[j].steps > 500){
+                    counter2++;
+                    strcpy(tempEnd,fitness[j].time);
+                    printf("%d",counter2);
+                }
+                else{
+                    if (counter2 > highest){
+                        strcpy(timeEnd, tempEnd);
+                        strcpy(timeStart, tempStart);
+                        highest = counter2;
+                    }
+                    counter2 = 0;
+                    break;
+                }
+            }
+        }
+    }
+    printf("Longest period start: %s %s\n", date, timeStart);
+    printf("Longest period end: %s %s\n", date, timeEnd);
+    return 0;
 }
 
 // Complete the main function
 int main() {
-    char option = Menu();
+    char option;
+    option=Menu();
 
+    char filename [100];
     FITNESS_DATA fitness[1000];
     int buffer_size = 500;
     char line_buffer[buffer_size];
     int counter = 0;
 
-    char filename [] = "FitnessData_2023.csv";
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        perror("");
-        return 1;
-    }
-    
-    char date[11];
-    char time[6];
-    char steps[10];
+    while (option != 'Q'){
+        if (option == 'A'){
+            printf("Input filename: ");
+            scanf("%s", filename);
+            FILE *file = fopen(filename, "r");
+            if (file == NULL) {
+                printf("Error: Could not find or open the file.\n");
+                return 1;
+            }
 
-    while (fgets(line_buffer, buffer_size, file) != NULL) {
-        tokeniseRecord(line_buffer,",",date,time,steps);
-        strcpy(fitness[counter].date,date);
-        strcpy(fitness[counter].time,time);
-        fitness[counter].steps = atoi(steps);
-        counter ++;
-    }
-    fclose(file);
+            char date[11];
+            char time[6];
+            char steps[10];
 
-    if (option == 'B'){
-        optionB(counter); 
+            while (fgets(line_buffer, buffer_size, file) != NULL) {
+                tokeniseRecord(line_buffer,",",date,time,steps);
+                strcpy(fitness[counter].date,date);
+                strcpy(fitness[counter].time,time);
+                fitness[counter].steps = atoi(steps);
+                counter ++;
+            }
+            fclose(file);
+            printf("File sucessfully loaded.\n");
+            Menu();
+            scanf("%c",&option);
+        }
+        if (option == 'B'){
+            optionB(counter);
+        }
+        else if (option == 'C') {
+            optionC(fitness, counter);
+        }
+        else if (option == 'D'){
+            optionD(fitness, counter);
+        }
+        else if (option =='E'){
+            optionE(fitness, counter);
+        }
+        else if (option == 'F'){
+            optionF(fitness, counter);
+        }
+        else{
+            printf("Invalid choice. Try again.\n");
+        }
+        Menu();
+        scanf("%c", &option);
     }
-    else if (option == 'C') {
-        optionC(fitness, counter);
-    }
-    else if (option == 'D'){
-        optionD(fitness, counter);
-    }
-    else if (option =='E'){
-        optionE(fitness, counter);
-    }
-    else if (option == 'Q'){
-        optionQ();
-    }
-    option = Menu();
-    printf("%c",option);
+    return 0;
 }
